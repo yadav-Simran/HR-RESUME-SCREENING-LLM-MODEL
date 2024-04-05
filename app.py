f08e04a7e3adb505650a523d38eb2ba9da681adb
Simran Yadav <123407453+yadav-Simran@users.noreply.github.com>
2024-04-05 14:38:34 +0530

    removed hardcoded api keys

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from utils import *
 import uuid
 import os
 
-#Creating session variables
 if 'unique_id' not in st.session_state:
     st.session_state['unique_id'] =''
 
@@ -15,14 +14,12 @@ def main():
     
     st.title("HR - Resume Screening Assistance")
     
-    # st.subheader("I can help you in resume screening process")
     
     st.markdown("<div style='text-align: center;'>" "<h6>Please paste the 'Job Description' here</h3>" "</div>", unsafe_allow_html=True)
     job_description = st.text_area("",key="1")
 
     pdf = st.file_uploader("Upload 2 or more resumes here", type=["pdf"],accept_multiple_files=True)
     
-    # document_count = st.text_input("Enter the number of 'RESUMES' to return",key="2")
     if pdf : 
         max_resumes = len(pdf) if pdf else 1
         document_count = st.selectbox("Select the number of 'RESUMES' to return", options=range(1, max_resumes))
@@ -35,42 +32,26 @@ def main():
 
             #Creating a unique ID, so that we can use to query and get only the user uploaded documents from PINECONE vector store
             st.session_state['unique_id']=uuid.uuid4().hex
-
-            #Create a documents list out of all the user uploaded pdf files
             final_docs_list=create_docs(pdf,st.session_state['unique_id'])
-
-            #Displaying the count of resumes that have been uploaded
             st.write("*Resumes uploaded* :"+str(len(final_docs_list)))
 
-            #Create embeddings instance
             embeddings=create_embeddings_load_data()
-
-            #Push data to PINECONE
-            # push_to_pinecone(os.getenv("pinecone_api_key"),"gcp-starter","genai-doc",embeddings,final_docs_list)
+            
             push_to_pinecone('pinecone_api_key',"gcp-starter","genai-doc",embeddings,final_docs_list)
 
-            #Fecth relavant documents from PINECONE
             relavant_docs=similar_docs(job_description,document_count,'pinecone_api_key',"gcp-starter","genai-doc",embeddings,st.session_state['unique_id'])
-
-            #t.write(relavant_docs)
-
-            #Introducing a line separator
+         
             st.write(":heavy_minus_sign:" * 30)
 
-            #For each item in relavant docs - we are displaying some info of it on the UI
             for item in range(len(relavant_docs)):
                 
                 st.subheader("Resume ranking ➡️ "+str(item+1))
 
-                #Displaying Filepath
                 st.write("**File** : "+relavant_docs[item][0].metadata['name'])
 
-                #Introducing Expander feature
                 with st.expander('Display the resume ✅'): 
                     st.info("**Match Score** : "+str(relavant_docs[item][1]))
-                    #st.write("***"+relavant_docs[item][0].page_content)
                     
-                    #Gets the summary of the current item using 'get_summary' function that we have created which uses LLM & Langchain chain
                     summary = get_summary(relavant_docs[item][0])
                     st.write("**Summary** : "+summary)
                     
@@ -81,6 +62,5 @@ def main():
             st.success("Hope I was able to save your time❤️")
 
 
-#Invoking main function
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ from langchain_pinecone import PineconeVectorStore
 import time
 
 
-#Extract Information from PDF file
 def get_pdf_text(pdf_doc):
     text = ""
     pdf_reader = PdfReader(pdf_doc)
@@ -21,16 +20,11 @@ def get_pdf_text(pdf_doc):
     return text
 
 
-
-# iterate over files in 
-# that user uploaded PDF files, one by one
 def create_docs(user_pdf_list, unique_id):
     docs=[]
     for filename in user_pdf_list:
         
         chunks=get_pdf_text(filename)
-
-        #Adding items to our list - Adding data & its metadata
         docs.append(Document(
             page_content=chunks,
             metadata={"name": filename.name,"id":filename.file_id,"type=":filename.type,"size":filename.size,"unique_id":unique_id},
@@ -38,51 +32,26 @@ def create_docs(user_pdf_list, unique_id):
 
     return docs
 
-
-#Create embeddings instance
 def create_embeddings_load_data():
     embeddings = OpenAIEmbeddings()
-    # embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
     return embeddings
 
-
-#Function to push data to Vector Store - Pinecone here
 def push_to_pinecone(pinecone_apikey,pinecone_environment,pinecone_index_name,embeddings,docs):
 
-    # pinecone.init(
-    # api_key=pinecone_apikey,
-    # environment=pinecone_environment
-    # )
-    
-    # Pinecone.from_documents(docs, embeddings, index_name=pinecone_index_name)
     
     docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=pinecone_index_name)
-    # Chroma.from_documents(docs, embeddings)
 
-#Function to pull infrmation from Vector Store - Pinecone here
 def pull_from_pinecone(pinecone_api_key,pinecone_environment,pinecone_index_name,embeddings):
     
     print("20secs delay...")
     time.sleep(20)
-    # pinecone.init(
-    # api_key=pinecone_apikey,
-    # environment=pinecone_environment
-    # )
 
     index_name = pinecone_index_name
 
     index = Pinecone.from_existing_index(index_name, embeddings)
     return index
 
-
-
-#Function to help us get relavant documents from vector store - based on user input
 def similar_docs(query,k,pinecone_api_key,pinecone_environment,pinecone_index_name,embeddings,unique_id):
-
-    # pinecone.init(
-    # api_key=pinecone_apikey,
-    # environment=pinecone_environment
-    # )
 
     index_name = pinecone_index_name
 
@@ -91,13 +60,8 @@ def similar_docs(query,k,pinecone_api_key,pinecone_environment,pinecone_index_na
     #print(similar_docs)
     return similar_docs
 
-
-# Helps us get the summary of a document
 def get_summary(current_doc):
-    # llm = OpenAI(temperature=0)
-    llm = ChatOpenAI(model_name="gpt-4-turbo-preview")  # language model
-
-    #llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature":1e-10})
+    llm = ChatOpenAI(model_name="gpt-4-turbo-preview")  
     chain = load_summarize_chain(llm, chain_type="map_reduce")
     summary = chain.run([current_doc])
 
